1c3280270d4a2f21b1e562b3e49b3256ece77895
Erko <matteo.ercolani@gmail.com>
2016-04-25 16:41:03 +0200

    refactor animations

--- project/home/views.js
+++ project/home/views.js
@@ -8,66 +8,9 @@ import FloatingActionButton from 'material-ui/lib/floating-action-button';
 import { AnimatedContent } from '../core/components';
 
 
-//export class HomeViewComplete extends BaseView {
-//  goToSearch() {
-//    this.response.navigate('search:main', {});
-//  }
-//
-//  goToProfile() {
-//    this.response.navigate('profile:main', {});
-//  }
-//
-//  render() {
-//
-//    return <div className="content-container">
-//      <div className="search" onClick={this.goToSearch.bind(this)} ref="search">
-//        <SearchComponent />
-//
-//      </div>
-//
-//      <div className="profile" onClick={this.goToProfile.bind(this)} ref="profile">
-//        <ProfileComponent />
-//      </div>
-//
-//    </div>;
-//  }
-//}
 const buttonStyle = {
   margin: 20
 };
-//
-//export class HomeView extends BaseView {
-//  goToSearch() {
-//    this.response.navigate('search:main', {});
-//  }
-//
-//  goToProfile() {
-//    this.response.navigate('profile:main', {});
-//  }
-//
-//  render() {
-//
-//    return <Row className="center-xs">
-//      <Col xs={6}>
-//        <div className="box-row" ref="searchContainer" onClick={this.goToSearch.bind(this)}>
-//          <FloatingActionButton style={buttonStyle} secondary={true}>
-//            <i className="fa fa-search"></i>
-//          </FloatingActionButton>
-//
-//          <h3>Cerca</h3>
-//        </div>
-//      </Col>
-//      <Col xs={6}>
-//        <div className="box-row" ref="profileContainer" onClick={this.goToProfile.bind(this)}>
-//          <FloatingActionButton style={buttonStyle} secondary={true}>
-//            <i className="fa fa-user"></i>
-//          </FloatingActionButton>
-//          <h3>Fatti trovare</h3>
-//        </div>
-//      </Col>
-//    </Row>;
-//  }
-//}
 
 
 export class HomeLeft extends BaseComponent {
