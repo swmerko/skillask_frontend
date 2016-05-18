f5fd4af016e8f25e7e954bbd8510587f796a2fe1
Carlo Bertini [WaYdotNET] <waydotnet@gmail.com>
2016-05-18 18:11:49 +0200

    update package and gulp task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,10 @@
-// generated on 2016-03-12 using generator-outlinejs 1.3.19
+// generated on 2016-05-14 using generator-outlinejs 1.3.20
 import gulp from 'gulp';
-import OutlineJsTasks from 'outlinejs/lib/utils/build/tasks';
-import browserify from 'browserify';
 import eslint from 'gulp-eslint';
+import browserify from 'browserify';
+
+import OutlineJsTasks from 'outlinejs/lib/utils/build/tasks';
+
 
 class GulpTasks extends OutlineJsTasks {
   getBrowserify(debug = false, forNode = false, watch = false, files = [this.projectJsEntry]) {
@@ -19,10 +21,4 @@ ojsTasks.load();
 
 gulp.task('default', ['ojs:clean'], () => {
   gulp.start('ojs:build');
-});
-
-//TODO: remove when pot task is fixed for ES7
-gulp.task('ojs:pot', (cb) => {
-  console.log('Bypassed until pot task is fixed for ES7');
-  cb();
 });
